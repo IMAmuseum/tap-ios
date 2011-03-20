c8fdc42e92cc6cd2f1d72d384e70a4ab0e5349f3
Charlie Moad <cwmoad@gmail.com>
2011-03-20 20:12:33 +0000

    added additional check for our audio/video stop reuse

--- Tap/Classes/StopFactory.h
+++ Tap/Classes/StopFactory.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 
 #import "Stop.h"
+#import "VideoStop.h"
 
 
 @interface StopFactory : NSObject {
--- Tap/Classes/StopFactory.m
+++ Tap/Classes/StopFactory.m
@@ -5,6 +5,21 @@
 
 +(id<Stop>)newStopForStopNode:(xmlNodePtr)stop
 {
+    // Check for Video/AudioStop cases where we always use the VideoStop
+    // This could be handled more elegantly later
+    if (xmlStrEqual(stop->name, (xmlChar*)"AudioStop"))
+    {
+        VideoStop *videoStop = [[VideoStop alloc] initWithStopNode:stop];
+        [videoStop setIsAudio:YES];
+        return videoStop;
+    }
+    else if (xmlStrEqual(stop->name, (xmlChar*)"VideoStop"))
+    {
+        VideoStop *videoStop = [[VideoStop alloc] initWithStopNode:stop];
+        [videoStop setIsAudio:NO];
+        return videoStop;
+    }
+    
     // Try to dynamically load the class with the matching name (e.g. ImageStop, StopGroup, etc)
     Class stopClass = NSClassFromString([NSString stringWithUTF8String:(char*)stop->name]);
     
